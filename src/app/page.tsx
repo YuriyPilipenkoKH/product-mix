7192a9598c03bf84a3d9869e769b5054c0fc0ee7
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-22 22:19:46 +0200

    fixed hydration reeor caused by background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <div
       className="hero min-h-screen"
-      style={{
-        backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-      }}>
+      // style={{
+      //   backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+      // }}
+      >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
