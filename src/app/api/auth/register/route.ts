ab2c10d564f2132a5abb0c35670c67f9f80adce8
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-22 11:18:42 +0200

    post request to register

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,7 +1,7 @@
 import { connectMongoDB } from "@/lib/mongo"
 import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
-
+import { hash} from "bcrypt-ts";
 
 export const POST = async (req: Request) => {
   try {
@@ -12,14 +12,24 @@ export const POST = async (req: Request) => {
         {status: 402}
       )
     }
+    const hashedPassword = await hash(password,10)
     await connectMongoDB()
     const newUser = prisma.user.create({
       data:{
-          name, email, password
+          name,
+          email, 
+          password: hashedPassword
         }
       })
+    return NextResponse.json(
+      {newUser},
+      {status: 201}
+    )  
   } 
   catch (error) {
-    
-  }
+    return NextResponse.json(
+      { message: "Failed to register"},
+      { status: 500 }
+    )
+    }
 }
