c7301ad3f795167bed944293709ee13b38313c7c
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-22 18:59:43 +0200

    to dashboard

--- src/actions/login-user.ts
+++ src/actions/login-user.ts
@@ -1,7 +1,6 @@
 'use server'
 
 import prisma from "@/lib/prisma";
-import {hashSync} from 'bcrypt-ts'
 import { signIn } from "../../auth";
 import { connectMongoDB } from "@/lib/mongo";
 import { revalidatePath } from "next/cache";
--- src/actions/register-user.ts
+++ src/actions/register-user.ts
@@ -4,20 +4,21 @@ import prisma from "@/lib/prisma";
 import {hashSync} from 'bcrypt-ts'
 import { signIn } from "../../auth";
 import { connectMongoDB } from "@/lib/mongo";
+import { revalidatePath } from "next/cache";
 
 export async function registerUser (formData: FormData)  {
   const name = formData.get('name') as string | null; // Explicitly cast as string | null
   const email = formData.get('email') as string | null;
   const password = formData.get('password') as string | null;
   const allowedEmails = process.env.ALLOWED_EMAILS?.split(',') || [];
-  // Optional: Throw error or handle null values
+
   if (!name || !email || !password) {
     throw new Error("All fields are required.");
   }
 
   try {
     await connectMongoDB();
-    
+
     if (allowedEmails.includes(email)) {
 
       // Check if a user already exists with the same email
@@ -52,6 +53,7 @@ export async function registerUser (formData: FormData)  {
       }
       // Exclude sensitive fields
       const { password: _, ...plainUser } = newUser;
+      revalidatePath('/dashboard');
       return { 
         success: true, 
         message: "User registered and logged in successfully.", 
