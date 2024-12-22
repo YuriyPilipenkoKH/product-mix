24608afbcf39e730c933c8e392d05d6f19d70303
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-22 19:13:46 +0200

    added website layout

---
+++ src/app/(website)/layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+
+interface Props {
+    children: ReactNode;
+  }
+
+function Layout({ children }: Props) {
+  return (
+    <div className='h-full flex flex-col items-center justify-center gap-2 bg-[var(--website-bg)]'>
+        <div className='flex gap-2'>
+          <h2>Website</h2>
+          {/* <LogoutButton/> */}
+        </div>
+        
+        {children}
+    </div>
+  )
+}
+
+export default Layout
