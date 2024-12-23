145a21d67f817dbbb2887dccb73281c2230052d6
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-23 22:12:51 +0200

    added links to navbar

--- src/components/Website/WebNavBar.tsx
+++ src/components/Website/WebNavBar.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const WebNavBar = () => {
   return (
-    <div>WebNavBar</div>
+    <nav className='flex items-center justify-center p-3 w-full '>
+      <Link href={''}>logo</Link>
+      <Link href={'/'}>home</Link>
+      <Link href={'/adminpanel'}>admin page</Link>
+    </nav>
   )
 }
 
