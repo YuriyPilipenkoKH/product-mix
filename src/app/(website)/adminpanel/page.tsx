40499f9a16d2b41dc19b95a6db6e4ece72dd2cf3
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-23 22:52:23 +0200

    closing admin page

--- src/app/(website)/adminpanel/page.tsx
+++ src/app/(website)/adminpanel/page.tsx
@@ -4,17 +4,15 @@ import { getSession } from "@/lib/getSession"
 const AdminPage =async () => {
   const session = await getSession()
   const user = session?.user
+  const userRole = user?.role;
   console.log(user);
   
   return (
-    <div>
-      <h2>Admin page</h2>
-      {/* <UsersList/> */}
-      {/* <UserRoleManager 
-      user={user} 
-      onRoleChange ={change}
-       /> */}
-    </div>
+    <>
+      {userRole === 'admin' && (
+        <h2>Admin page</h2>
+      )}
+    </>
   )
 }
 export default AdminPage
