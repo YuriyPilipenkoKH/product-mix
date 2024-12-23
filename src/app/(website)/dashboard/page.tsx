0b3eccb61347a21e08ce2e227546dc271504091b
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-23 11:16:29 +0200

    Pass session to children explicitly

--- src/app/(website)/dashboard/page.tsx
+++ src/app/(website)/dashboard/page.tsx
@@ -1,23 +1,26 @@
-import capitalize from '@/lib/capitalize'
-import { getSession } from '@/lib/getSession'
-import { redirect } from 'next/navigation'
+import capitalize from '@/lib/capitalize';
+import { Session } from 'next-auth';
+import React from 'react';
 
-import React from 'react'
-
-const DashboardPage = async () => {
-  const session = await getSession()
-  const user = session?.user
-  const userName= user?.name
-  const userEmail= user?.email
-  if (!session) {
-    redirect('/login')
-  }
-  return (
-    <div className='grid gap-1' >
-      <p>Logged in as  {userName && capitalize(userName)  || 'User'}</p>
-      <p>with email {userEmail}</p>
-    </div>
-  )
+interface DashboardPageProps {
+  session: Session
 }
 
-export default DashboardPage
+
+const DashboardPage:React.FC<DashboardPageProps> =  ({
+  session
+}) => {
+
+  const user = session.user;
+  const userName = user?.name;
+  const userEmail = user?.email;
+
+  return (
+    <div className="grid gap-1">
+      <p>Logged in as {userName && capitalize(userName) || 'User'}</p>
+      <p>with email {userEmail}</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,21 +1,28 @@
+import { getSession } from '@/lib/getSession';
+import { redirect } from 'next/navigation';
+import { Session } from 'next-auth';
+import React from 'react';
 import { LogoutButton } from '@/components/Button/Button';
-import { ReactNode } from 'react'
-
 interface Props {
-    children: ReactNode;
-  }
-
-function Layout({ children }: Props) {
-  return (
-    <div className='h-full flex flex-col items-center justify-center gap-2 bg-[var(--website-bg)]'>
-        <div className='flex gap-2'>
-          <h2>Website</h2>
-          <LogoutButton/>
-        </div>
-        
-        {children}
-    </div>
-  )
+  children: React.ReactElement<{ session: Session }>; // Specify that children accepts session
 }
 
-export default Layout
+async function Layout({ children }: Props) {
+  const session = await getSession();
+
+  if (!session) {
+    redirect('/login'); // Redirect if no session
+  }
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center gap-2 bg-[var(--website-bg)]">
+      <div className="flex gap-2">
+        <h2>Website</h2>
+        <LogoutButton />
+      </div>
+      {React.cloneElement(children, { session })}
+    </div>
+  );
+}
+
+export default Layout;
