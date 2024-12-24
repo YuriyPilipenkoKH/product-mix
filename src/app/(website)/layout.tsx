23967489dbc8eee8963a6fbf3499f3b92e0ddb40
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-24 09:48:58 +0200

    protect layout

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -14,16 +14,18 @@ async function Layout({ children }: Props) {
   }
 
   return (
-    <div className="h-full flex flex-col items-center justify-center gap-2 bg-[var(--website-bg)]">
-      <WebNavBar session={session} />
-      <div className="flex gap-2">
-        <h2>Website</h2>
-        
-      </div>
-      {/* {React.cloneElement(children, { session })} */}
-      {children}
-
-    </div>
+    <>
+      {session && (
+        <div className="h-full flex flex-col items-center justify-center gap-2 bg-[var(--website-bg)]">
+          <WebNavBar session={session} />
+          <div className="flex gap-2">
+            <h2>Website</h2>      
+          </div>
+          {/* {React.cloneElement(children, { session })} */}
+          {children}
+        </div>
+      )}
+    </>
   );
 }
 
