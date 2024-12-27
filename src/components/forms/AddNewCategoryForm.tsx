dc960f60f0ced528ba0769ef7a7aa865c8c9c30b
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-27 12:27:28 +0200

    template for try-catch block

--- src/components/forms/AddNewCategoryForm.tsx
+++ src/components/forms/AddNewCategoryForm.tsx
@@ -2,6 +2,7 @@ import { addNewCategorySchema, addNewCategorySchemaType } from '@/models/addCate
 import { zodResolver } from '@hookform/resolvers/zod'
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
 
 interface AddNewCategoryFormProps {
 	creator: string
@@ -34,6 +35,13 @@ export const AddNewCategoryForm: React.FC<AddNewCategoryFormProps> = ({
       const formData = new FormData();
 			formData.append('name', data.name);
 			formData.append('creator', creator);
+      try {
+        
+      } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+        toast.error(`An error occurred: ${errorMessage}`);
+        setLogError(errorMessage)
+      }
     }
   return (
     <div>AddNewCategoryForm</div>
