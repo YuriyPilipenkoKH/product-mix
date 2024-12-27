437c92af84901ed1d0418c8ba5df589caafaec6b
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-27 13:35:03 +0200

    completed userinfo

--- src/components/User/UserInfo.tsx
+++ src/components/User/UserInfo.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { getSession } from '@/actions/getSession';
 import capitalize from '@/lib/capitalize';
+import { redirect } from 'next/navigation';
+
 
 const UserInfo = async () => {
     const session = await getSession();
+      if (!session) {
+        redirect('/login'); 
+      }
     const { user } = session;
   return (
-    <div>UserInfo</div>
+    <div>
+      <p>Logged in as {capitalize(user?.name) || "User"}</p>
+      <p>With email {user?.email}</p>
+      <p>With role {user?.role}</p>
+    </div>
   )
 }
 
