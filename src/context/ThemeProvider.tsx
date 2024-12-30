2bbf4e88a35e2506029b04c773fb45bf1bccf504
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-30 04:40:37 +0200

    ThemeProvider template

---
+++ src/context/ThemeProvider.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React, { ReactNode } from 'react'
+
+function ThemeProvider({children} : {children:ReactNode}) {
+  return (
+    <div>ThemeProvider</div>
+  )
+}
+
+export default ThemeProvider
