a61c5c7dc1554806f6fd7db24cdd80bf77a41f90
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-30 04:54:34 +0200

    retrieving theme from localStorage

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,10 +1,17 @@
 "use client"
-import React, { ReactNode, useState } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 import ThemeContext from './ThemeContext'
 
 function ThemeProvider({children} : {children:ReactNode}) {
   const [theme, setTheme] = useState('dark')
   const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+    const storedTheme = localStorage.getItem('theme') || 'dark'
+    setTheme(storedTheme)
+      }, [])
+  
   return (
     <ThemeContext.Provider value={{ 
       theme, 
