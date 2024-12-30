98d97f97022bfdc03a4b5f752743221421cf4ab6
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-30 04:49:17 +0200

    binded ThemeContext.Provider

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,12 +1,21 @@
 "use client"
 import React, { ReactNode, useState } from 'react'
+import ThemeContext from './ThemeContext'
 
 function ThemeProvider({children} : {children:ReactNode}) {
   const [theme, setTheme] = useState('dark')
   const [isMounted, setIsMounted] = useState(false)
   return (
-    <div>ThemeProvider</div>
+    <ThemeContext.Provider value={{ 
+      theme, 
+      setTheme,
+      isMounted,
+      setIsMounted
+      }}>
+      {children}
+    </ThemeContext.Provider>
   )
 }
+
 
 export default ThemeProvider
