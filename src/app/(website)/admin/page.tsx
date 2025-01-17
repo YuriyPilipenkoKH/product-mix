3d0b464e9b29e640fa072c3ec9ead0c850bae8a2
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-17 12:37:11 +0200

    updated admin page

--- src/app/(website)/admin/page.tsx
+++ src/app/(website)/admin/page.tsx
@@ -1,21 +1,31 @@
 import { redirect } from "next/navigation";
 import { auth } from "../../../../auth";
 import capitalize from "@/lib/capitalize";
+import { watchRole } from "@/actions/watch-role";
+import Link from "next/link";
 
 
 const AdminPage = async () => {
 
-    const session = await auth();
-    const user = session?.user
-    const userRole = session?.user?.role
-    if(userRole !== 'admin') redirect('/dashboard')
+  const session = await auth();
+  if (!session) redirect('/login'); 
+
+  const email = session?.user?.email;
+  if (!email) redirect('/dashboard');
+
+  const formData = new FormData();
+  formData.append("email", email);
+
+  const userRole = await watchRole(formData); // Call server action
+  if (userRole !== 'admin') redirect('/dashboard'); 
   
   return (
     <div>
       <h2>Admin page</h2>
-      <p>Logged in as {capitalize(user?.name) }</p>
-      <p>With email {user?.email}</p>
-      <p>With role {user?.role}</p>
+      <Link href={'/dashboard'}>dashboard Page</Link>
+      <div>name{' '}{capitalize(session?.user?.name)}</div>
+      <div>role{' '}{userRole}</div>
+      <div>photo{' '}{session?.user?.image}</div>
     </div>
   )
 }
