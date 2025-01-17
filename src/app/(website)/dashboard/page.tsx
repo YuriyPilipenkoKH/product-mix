efb29c6696261260c18f04cd314064c6b75b5457
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-17 19:04:24 +0200

    svgs added

--- src/app/(website)/dashboard/page.tsx
+++ src/app/(website)/dashboard/page.tsx
@@ -1,16 +1,51 @@
-import UserInfo from '@/components/User/UserInfo';
-import React from 'react';
-import { auth } from '../../../../auth';
 
 
-const DashboardPage =  async() => {
-const session = await auth();
-console.log(session)
+import { orders } from '@/data/orders';
+import Image from 'next/image';
+
+export default function DashboardPage() {
+
   return (
-    <div className="grid gap-1 h-full">
-        <UserInfo/>
+    <div className="grid gap-4 md:gap-8">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div>
+          <h2>Today&apos;s Orders</h2>
+          <p>
+            Your orders for today. Keep up the good work!
+          </p>
+        </div>
+        <div>
+          <div className="flex flex-col gap-4">
+            {orders.map((order) => (
+              <div className='card' key={order.id}>
+                <div className="flex flex-col justify-between gap-4 py-4 md:flex-row md:items-center">
+                  <div className="flex items-center gap-4">
+                    <Image
+                      alt="Image"
+                      className="rounded-md object-cover"
+                      height="64"
+                      src={order.image || '/deer.svg'}
+                      style={{
+                        aspectRatio: '64/64',
+                        objectFit: 'cover',
+                      }}
+                      width="64"
+                    />
+                    <div className="grid flex-1 gap-1">
+                      <h3 className="font-semibold">{order.name}</h3>
+                      <p className="text-sm text-gray-500">
+                        Order ID: {order.orderId}
+                      </p>
+                    </div>
+                  </div>
+
+                  <button >Track</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
-
-export default DashboardPage;
+}
---
+++ src/data/orders.ts
@@ -0,0 +1,20 @@
+export   const orders = [
+  {
+    id: 1,
+    name: 'Crispy Chicken Burger',
+    image: '/pizza.svg',
+    orderId: '#123456',
+  },
+  {
+    id: 2,
+    name: 'Iced Latte',
+    image: '/pizza.svg',
+    orderId: '#654321',
+  },
+  {
+    id: 3,
+    name: 'Pepperoni Pizza',
+    image: '/pizza.svg',
+    orderId: '#987654',
+  },
+];
