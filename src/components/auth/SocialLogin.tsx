7e7bcdbd03b2858e6b32ec2e879c15910ce3d69f
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-17 12:28:12 +0200

    added purple color variable and updated border styles in authentication forms

--- src/components/auth/SocialLogin.tsx
+++ src/components/auth/SocialLogin.tsx
@@ -3,7 +3,7 @@ import SignInButton from '../Button/SignInButton'
 
 const SocialLogin = () => {
   return (
-      <div className='flex flex-col gap-5 p-2 w-[400px]'>
+      <div className='flex flex-col gap-5 px-2 pt-8 w-[400px]'>
         <SignInButton provider='google' />
         <SignInButton provider='github' />
       </div> 
