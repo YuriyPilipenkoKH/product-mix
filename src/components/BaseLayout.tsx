f54b97ea8e8ed6cb7f96d1281d75a1076083f36d
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-20 12:20:15 +0200

    postinstall scpipt for deployment

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -6,6 +6,8 @@ import {ReactNode} from 'react';
 import Navigation from './Navigation';
 import { Toaster } from 'react-hot-toast';
 import { options } from '@/lib/hotToast';
+import { SessionProvider } from 'next-auth/react';
+import { auth } from '@/auth';
 
 
 const inter = Inter({subsets: ['latin']});
@@ -19,16 +21,18 @@ export default async function BaseLayout({children, locale}: Props) {
   // Providing all messages to the client
   // side is the easiest way to get started
   const messages = await getMessages();
-
+  const session = await auth();
   return (
     <html className="h-full" lang={locale} suppressHydrationWarning>
       <body className={clsx(inter.className, 'flex h-full flex-col')}>
         <NextIntlClientProvider messages={messages}>
-          {children}
-          <Toaster 
-          position="top-center" 
-          toastOptions={options} 
-          gutter={24} />
+         <SessionProvider session={session}>
+            {children}
+            <Toaster 
+            position="top-center" 
+            toastOptions={options} 
+            gutter={24} />
+          </SessionProvider>
         </NextIntlClientProvider>
       </body>
     </html>
