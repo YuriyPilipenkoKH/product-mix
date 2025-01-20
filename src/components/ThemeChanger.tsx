afa564bbe39c3fbc031489952819237a72b53c90
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-20 13:03:24 +0200

    modified ThemeChanger component

--- src/components/ThemeChanger.tsx
+++ src/components/ThemeChanger.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { PiHeadlightsBold } from "react-icons/pi";
+import { BsMoonStars } from "react-icons/bs";
 
 export default function ThemeChanger() {
+  const [mounted, setMounted] = useState<boolean>(false)
   const [theme, setTheme] = useState<'light' | 'dark'>('light');
 
   useEffect(() => {
+    setMounted(true)
     // Apply the theme to the HTML element
     document.documentElement.setAttribute('data-theme', theme);
   }, [theme]);
@@ -14,13 +18,18 @@ export default function ThemeChanger() {
     setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
   };
 
+  if (!mounted) return (
+    <div className='placeholder w-9 h-9 bg-transparent'>
+    </div>
+  )
+
   return (
     <button
       className="btn btn-primary"
       onClick={toggleTheme}
       aria-label="Toggle Theme"
     >
-      {theme === 'light' ? '🌙 Dark Mode' : '☀️ Light Mode'}
+      {theme === 'light' ? <PiHeadlightsBold/> :  <BsMoonStars/>}
     </button>
   );
 }
