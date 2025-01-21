19b7afb2d56827359faa641e62e3d807ee7931e9
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-21 09:49:06 +0200

    refactored login-user

--- src/actions/login-user.ts
+++ src/actions/login-user.ts
@@ -4,11 +4,13 @@ import prisma from "../../prisma/prisma";
 import { connectMongoDB } from "@/lib/mongo";
 import { revalidatePath } from "next/cache";
 import { compare } from 'bcrypt-ts'
+import { getTranslations } from "next-intl/server";
 
 export const loginUser = async(formData: FormData) => {
   const rawFormData = Object.fromEntries(formData)
   console.log("rawFormData",rawFormData);
   const email = formData.get('email') as string | null;
+  const t = await getTranslations( 'Login-user'); 
   const password = formData.get('password') as string | null;
 
   if ( !email || !password ) {
@@ -22,15 +24,15 @@ export const loginUser = async(formData: FormData) => {
     });
   
     if (!user) {
-      return { success: false, error: "User not found" };
+      return { success: false, error: t("userNotFound") };
     }
 
     if (user.password) {
       const isPasswordValid = await compare(password, user.password); 
       if (!isPasswordValid) {
-        return { success: false, error: "Invalid login credentials" };
+        return { success: false, error: t("invalidLogin") };
       }
-    }else return  { success: false, error: "Couldn't find password " }
+    }else return  { success: false, error: t("missingPassword") }
 
     revalidatePath('/dashboard');
     return { success: true, user: {name: user.name, email: user.email}};
@@ -38,7 +40,9 @@ export const loginUser = async(formData: FormData) => {
     }
      catch (error) {
       console.error('Error occurred while registering:', error);
-      const errorMessage = error instanceof Error ? error.message : "An unexpected error occurred";
+      const errorMessage = error instanceof Error 
+      ? error.message 
+      : t("unexpectedError");
       return { success: false, error: errorMessage }
     }
 }
