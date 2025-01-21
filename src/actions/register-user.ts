293ca032e9ec25a8426453ef7e899bfa74f7784d
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-21 09:40:17 +0200

    refactored server action in order to translate all messages

--- src/actions/register-user.ts
+++ src/actions/register-user.ts
@@ -4,11 +4,13 @@ import prisma from "../../prisma/prisma";
 import {hashSync} from 'bcrypt-ts'
 import { connectMongoDB } from "@/lib/mongo";
 import { revalidatePath } from "next/cache";
+import { getTranslations } from "next-intl/server";
 
 export async function registerUser (formData: FormData)  {
   const name = formData.get('name') as string | null; // Explicitly cast as string | null
   const email = formData.get('email') as string | null;
   const password = formData.get('password') as string | null;
+  const t = await getTranslations( 'Register-user'); 
   const allowedEmails = process.env.ALLOWED_EMAILS?.split(',') || [];
 
   if (!name || !email || !password) {
@@ -26,7 +28,7 @@ export async function registerUser (formData: FormData)  {
       });
     
       if (existingUser) {
-        return { success: false, error: "Email is already registered." };
+        return { success: false, error: t('emailAlreadyRegistered') };
       }
       const hashedPassword =  hashSync(password)
        // Create the user in the database
@@ -45,17 +47,19 @@ export async function registerUser (formData: FormData)  {
       revalidatePath('/dashboard');
       return { 
         success: true, 
-        message: "User registered and logged in successfully.", 
+        message: t("userRegistered"), 
         user: plainUser
       };
     } else {
-      return { success: false, error: "Email is not allowed" };
+      return { success: false, error: t("emailNotAllowed") };
     }     
 
   }
    catch (error) {
     console.error('Error occurred while registering:', error);
-    const errorMessage = error instanceof Error ? error.message : "An unexpected error occurred";
+    const errorMessage = error instanceof Error 
+    ? error.message 
+    : t("AunexpectedErrord");
     return { success: false, error: errorMessage }
   }
 
