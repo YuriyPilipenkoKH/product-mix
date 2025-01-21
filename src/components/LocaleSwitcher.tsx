72b890cc8d603a183be4f5d62a87bdee2a3ee0c3
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-21 14:14:17 +0200

    finished markup for custom select

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,18 +1,28 @@
+'use client';
 import {useLocale, useTranslations} from 'next-intl';
 import {routing} from '@/i18n/routing';
 import LocaleSwitcherSelect from './LocaleSwitcherSelect';
+import { MouseEvent } from 'react';
 
 
 export default function LocaleSwitcher() {
   const t = useTranslations('LocaleSwitcher');
   const locale = useLocale();
+  const click =(e: MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLButtonElement;
+    console.log(target.id);
+  }
 
   return (
-    <LocaleSwitcherSelect defaultValue={locale} label={t('label')} >
+    <LocaleSwitcherSelect defaultValue={locale} label={t('current')} >
       {routing.locales.map((cur) => (
-        <option key={cur} value={cur} className='bg-transparent'>
+        <button 
+        key={cur} 
+        id={cur} 
+        className='bg-transparent text-[var(--text-color)] hover:text-[var(--text-color-hover)]'
+        onClick={click}>
           {t('locale', {locale: cur})}
-        </option>
+        </button>
       ))}
     </LocaleSwitcherSelect>
   );
